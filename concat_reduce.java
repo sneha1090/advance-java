6f261bc418ecdb621987e9c83e1ab35169bb6188
sneha1090 <shikhasingh9559754011@gmail.com>
2026-03-25 15:49:12 +0530

    commit sumofeven_reduce

--- concat_reduce.java
+++ concat_reduce.java
@@ -1,3 +1,12 @@
-public class concat_reduce {
-    
+import java.util.*;
+
+public class concat_reduce
+ {
+    public static void main(String[] args)
+     {
+         List<String>list=Arrays.asList("sneha","singh");
+         String result=list.stream()
+         .reduce("",(a,b)->a+b);
+         System.out.println(result);
+    }
 }
